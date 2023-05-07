05046db97dc0101a0278268a3c643eecba63b965
lemonlove7 <56328995+lemonlove7@users.noreply.github.com>
2023-05-07 11:18:44 +0800

    Create qc.py

---
+++ lib/qc.py
@@ -0,0 +1,46 @@
+import re
+import tldextract
+from colorama import init, Fore, Style
+re_lists=[]
+
+init()
+def pass403_qc():
+    try:
+        print(Fore.GREEN + Style.BRIGHT +'\nRemove invalid results with the same page length')
+        print(Style.RESET_ALL)
+        with open('bypass403_url.txt') as f:
+            url = f.read()
+        file_domain = tldextract.extract(url).domain
+        with open(file_domain + '.txt') as f:
+            f1 = f.readlines()
+        for i in f1:
+            i = i.replace('\n', '').replace('\r', '')
+            if 'Header= {' in i:
+                # print(i)
+                re_i = re.findall('SIZE: (.*?)---Header=', i)
+                re_str = "".join(re_i)
+            if 'Header= {' not in i:
+                re_str = i[i.rfind('SIZE: '):]
+            # re_str="".join(re_i)
+            if re_str != '':
+                re_lists.append(re_str)
+        # print(re_lists)
+        list2 = list(set(re_lists))
+
+        for ii in list2:
+            for p in f1:
+                p = p.replace('\n', '')
+                if ii in p:
+                    if "---Header= {" in p:
+                        p = p.replace('---', '\n')
+                        p = p+'\n'
+                    if "STATUS: 403" in p:
+                        pass
+                    elif "STATUS: 404" in p:
+                        pass
+                    else:
+                        print(p)
+                    break
+    except:
+        print(Fore.GREEN + Style.BRIGHT+'\nFailed to remove duplicates!')
+        print(Style.RESET_ALL)
