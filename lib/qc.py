acec7bfb398e70f0631f9d2e826f119a11d541b7
lemonlove7 <56328995+lemonlove7@users.noreply.github.com>
2023-05-09 12:01:34 +0800

    Update qc.py

--- lib/qc.py
+++ lib/qc.py
@@ -1,3 +1,4 @@
+import os
 import re
 import tldextract
 from colorama import init, Fore, Style
@@ -5,42 +6,47 @@ re_lists=[]
 
 init()
 def pass403_qc():
+    urls=[]
     try:
-        print(Fore.GREEN + Style.BRIGHT +'\nRemove invalid results with the same page length')
-        print(Style.RESET_ALL)
+        print(Fore.GREEN + Style.BRIGHT +'\nRemove invalid results with the same page length'+Style.RESET_ALL)
         with open('bypass403_url.txt') as f:
             url = f.read()
-        file_domain = tldextract.extract(url).domain
-        with open(file_domain + '.txt') as f:
-            f1 = f.readlines()
-        for i in f1:
-            i = i.replace('\n', '').replace('\r', '')
-            if 'Header= {' in i:
-                # print(i)
-                re_i = re.findall('SIZE: (.*?)---Header=', i)
-                re_str = "".join(re_i)
-            if 'Header= {' not in i:
-                re_str = i[i.rfind('SIZE: '):]
-            # re_str="".join(re_i)
-            if re_str != '':
-                re_lists.append(re_str)
-        # print(re_lists)
-        list2 = list(set(re_lists))
+        urls.append(url)
+        with open('jsfind403list.txt') as js_f:
+            js_url=js_f.readlines()
+        for js_i in js_url:
+            js_i=js_i.replace('\n','').replace('\r','')
+            urls.append(js_i)
 
-        for ii in list2:
-            for p in f1:
-                p = p.replace('\n', '')
-                if ii in p:
-                    if "---Header= {" in p:
-                        p = p.replace('---', '\n')
-                        p = p+'\n'
-                    if "STATUS: 403" in p:
-                        pass
-                    elif "STATUS: 404" in p:
-                        pass
-                    else:
-                        print(p)
-                    break
+        for url1 in urls:
+            file_domain = tldextract.extract(url1).domain
+            with open(file_domain + '.txt') as f:
+                f1 = f.readlines()
+            for i in f1:
+                i = i.replace('\n', '').replace('\r', '')
+                if 'Header= {' in i:
+                    re_i = re.findall('SIZE: (.*?)---Header=', i)
+                    re_str = "".join(re_i)
+                if 'Header= {' not in i:
+                    re_str = i[i.rfind('SIZE: '):]
+                if re_str != '':
+                    re_lists.append(re_str)
+            list2 = list(set(re_lists))
+
+            for ii in list2:
+                for p in f1:
+                    p = p.replace('\n', '')
+                    if ii in p:
+                        if "---Header= {" in p:
+                            p = p.replace('---', '\n')
+                            p = p+'\n'
+                        if "STATUS: 403" in p:
+                            pass
+                        elif "STATUS: 404" in p:
+                            pass
+                        else:
+                            print(p)
+                        break
+            os.remove(file_domain + '.txt')
     except:
-        print(Fore.GREEN + Style.BRIGHT+'\nFailed to remove duplicates!')
-        print(Style.RESET_ALL)
+        pass
