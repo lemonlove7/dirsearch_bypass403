f6b91fa1afb16b4debd007e4230dd9733bf61b66
lemonlove7 <56328995+lemonlove7@users.noreply.github.com>
2023-05-09 11:58:21 +0800

    Update dirsearch.py

--- dirsearch.py
+++ dirsearch.py
@@ -25,6 +25,7 @@ from lib.qc import pass403_qc
 
 
 import threading
+from concurrent.futures import ThreadPoolExecutor
 import queue
 
 
@@ -86,21 +87,19 @@ def bypass():
 
 def run_bypass403():
     size = os.path.getsize('403list.txt')
+    size_js=os.path.getsize('jsfind403list.txt')
     from lib.core.options import parse_options
     if (parse_options()['bypass']) == None:
         pass
     else:
         bp403="".join(parse_options()['bypass'])
         if bp403 =='yes':
-            if size == 0:
-                print(Fore.GREEN + Style.BRIGHT + 'No 403 status code present!')
-                print(Style.RESET_ALL)
+            if size ==0 and size_js ==0:
+                print(Fore.GREEN + Style.BRIGHT + 'No 403 status code present!'+Style.RESET_ALL)
             else:
-                print(Fore.GREEN + Style.BRIGHT+'Start 403bypass!')
-                print(Style.RESET_ALL)
+                print(Fore.GREEN + Style.BRIGHT+'Start 403bypass!'+Style.RESET_ALL)
                 with open('403list.txt',) as f1:
                     list_403=f1.readlines()
-                #for path_403 in tqdm(list_403, desc='Bypassing 403 errors', unit=' files'):
                 for path_403 in list_403:
                     path_403=path_403.replace('\n','').replace('\r','')
                     q.put(path_403)
@@ -113,12 +112,64 @@ def run_bypass403():
                     t.start()
                 for t in thread_list:
                     t.join()
+
+                if size_js == 0:
+                    pass
+                else:
+                    try:
+                        with open('jsfind403list.txt') as js1:
+                            jsf=js1.readlines()
+                        def process_ff(ff):
+                            ff = ff.replace('\n', '').replace('\r', '')
+                            num_slashes = ff.count('/')
+                            if num_slashes == 2:
+                                ff = ff + '/'
+                            split_url = ff.split("/")
+                            js_url = "/".join(split_url[:3])
+                            js_path = split_url[3]
+                            if js_path == '':
+                                js_path = '/'
+                            argument = Arguments(js_url, None, js_path, None)
+                            program = Program(argument.return_urls(), argument.return_dirs())
+                            program.initialise()
+
+                        def js_403():
+                            with ThreadPoolExecutor() as executor:
+                                executor.map(process_ff, jsf)
+                        js_403()
+                    except:
+                        pass
                 pass403_qc()
+
         else:
             pass
 
 
+def jsfind():
+    import lib.JSFinder
+    from lib.core.options import parse_options
+    if (parse_options()['jsfind']) == None:
+        pass
+    else:
+        jsf="".join(parse_options()['jsfind'])
+        if jsf=='yes':
+            print(Fore.GREEN + Style.BRIGHT+"Start JsFind!"+Style.RESET_ALL)
+            url="".join(parse_options()['urls'])
+            urls = lib.JSFinder.find_by_url(url)
+            lib.JSFinder.giveresult(urls, url)
+        else:
+            pass
+
+
+def hhh():
+    open("403list.txt", 'w').close()
+    open('jsfind403list.txt','w').close()
+
+
 def main():
+
+    hhh()
+
     from lib.core.options import parse_options
 
     options.update(parse_options())
@@ -127,7 +178,10 @@ def main():
 
     Controller()
 
+    jsfind()
+
     run_bypass403()
+
 
 
 
