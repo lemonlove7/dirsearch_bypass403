2ab2cafabe6f0e7bc034ac509a011db2d28db133
lemonlove7 <56328995+lemonlove7@users.noreply.github.com>
2023-05-12 16:13:19 +0800

    Update ehole.py

--- ehole/ehole.py
+++ ehole/ehole.py
@@ -2,24 +2,20 @@ import re,os,sys,csv
 from urllib.parse import urlparse
 
 def start_ehole():
-    #获取当前路径
     path_get=os.getcwd()
     open(path_get+'/ehole/ehole.txt','w').close()
     with open('dir_file_path.txt') as f:
         f1=f.read()
 
-    #print(f1)
     with open(f1) as d:
         d1=d.readlines()
     for dd in d1:
         dd=dd.replace('\n','').replace('\r','')
-        reg = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'  # reg = 'https?://(?:[-\w.]|(?:%[\da-fA-F]{2}))+'url = re.findall(reg, data)print(url)
+        reg = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
         url=re.findall(reg,dd)
         try:
-            #print(url[0])
             with open(path_get+'/ehole/ehole.txt','a+') as ehole:
                 ehole.write(url[0]+'\n')
-            #urls.append(url[0])
         except:
             pass
 
@@ -31,7 +27,6 @@ def start_ehole():
         file_csv = open("reports/" + domain1 + '.csv', 'r', encoding='GB18030')
         rows = csv.reader(file_csv)
         for row in rows:
-            #print(row[0])
             if 'http' in row[0]:
                 try:
                     with open(path_get + '/ehole/ehole.txt', 'a+') as ehole:
